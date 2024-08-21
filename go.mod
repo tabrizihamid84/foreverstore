e917d8609e85c65686db34953efeb34396e1919b
Hamid Tabrizi <tabrizihamid6367@gmail.com>
2024-08-21 23:29:10 +0330

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tabrizihamid84/foreverstore
+
+go 1.23.0
---
+++ p2p/transport.go
@@ -0,0 +1,12 @@
+package p2p
+
+// Peer is an interface that represents the remote node
+type Peer interface{
+
+}
+
+
+// Transport is anything that can handles communication between the nodes in the network
+type Transport interface {
+
+}
